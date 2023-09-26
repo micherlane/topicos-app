d14de803d3d33167e65759b4deb0067dcc3eb512
) <testercod3@gmail.com>
2023-09-26 09:20:42 -0300

    Estrutura da página.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
 
 function App() {
   return (
-    <>
-    </>
+   <div>
+      <Header/>
+      <main></main>
+      <Footer/>
+   </div>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header(){
+    return ( 
+        <header>
+            <h1>Tópicos</h1>
+        </header>
+    )
+}
