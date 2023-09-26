2b4efee925d85268d4f458722410107d66635d4d
Micherlane Silva <micherlanerod@gmail.com>
2023-09-26 19:38:40 -0300

    .

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,10 @@
+import { AiOutlineCopyrightCircle } from 'react-icons/ai';
 import style from './style.module.css';
 
 export function Footer(){
     return (
         <footer className={style.footer}>
-            <p>Desenvolvido pela Equipe SoLaMente.</p>
+            <p><AiOutlineCopyrightCircle color='#FFF'/> Desenvolvido pela Equipe SoLaMente.</p>
         </footer>
     )
 }
