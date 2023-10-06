a2e4190c110f08fbee3350ca17b678022eadc663
SoLaMente Dev <testercod3@gmail.com>
2023-10-06 10:41:31 -0300

    Melhorada a leitura do reducer

--- src/reducers/likes_deslikes_reduce.ts
+++ src/reducers/likes_deslikes_reduce.ts
@@ -36,35 +36,30 @@ export const calcularPorcentagemLikes = (topico: Topico) => {
 
 export function reducer(state: State, action: Action): State {
     switch (action.type) {
-      case ActionType.LIKE:
+      case ActionType.LIKE:{
+        const topico = {...state.topico, like: state.topico.like + 1,} 
+  
+        return {
+          ...state,
+          topico,
+          porcentagemLikes: calcularPorcentagemLikes(topico),
+        };
+      }
 
+  
+      case ActionType.DESLIKE:{
+        const topico =  {...state.topico, deslike: state.topico.deslike + 1,}
+        
         return {
           ...state,
-          topico: {
-              ...state.topico,
-              like: state.topico.like + 1,
-          },
-          porcentagemLikes: calcularPorcentagemLikes({
-            ...state.topico,
-            like: state.topico.like + 1,
-          }),
+          topico,
+          porcentagemLikes: calcularPorcentagemLikes(topico),
         };
-  
-      case ActionType.DESLIKE:
-        return {
-          ...state,
-          topico: {
-            ...state.topico,
-            deslike: state.topico.deslike + 1,
-          },
-          porcentagemLikes: calcularPorcentagemLikes({
-            ...state.topico,
-            deslike: state.topico.deslike + 1,
-          }),
-        };
-  
-      default:
+      }
+
+      default:{
         return state;
+      }
     }
   }
   
